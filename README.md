01deded0d59daf8f3c7231d77f7f7b49cd26b238
Dixon <zdixon91.zd@gmail.com>
2022-03-20 12:45:24 -0500

    comments added

--- README.md
+++ README.md
@@ -4,12 +4,3 @@ In this project I am to ensure:
 * There are series of prompts asking for if lowercase, uppercase. numeric and/or special characters should be included
 * A prompt asking for the length of the password between 8 and 128 characters
 * The generated password displays on page
-
-I named this project arbitray latch due to arbitray meaning without reason or pattern.
-
-![Screenshot 2022-03-20 120035](https://user-images.githubusercontent.com/94766160/159173804-2450b18a-81fd-4f27-8f09-31738e50308e.png)
-![Screenshot 2022-03-20 120109](https://user-images.githubusercontent.com/94766160/159173808-3df9480c-455e-440d-8114-67c31c73e00a.png)
-![Screenshot 2022-03-20 115953](https://user-images.githubusercontent.com/94766160/159173815-644d151f-b7b4-4a4c-8edf-3ad238800ee4.png)
-
-
-Link to the depolyed page: https://zdsmu22.github.io/arbitrary-latch/
--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ let lower = "abcdefghijklmnopqrstuvwxyz";
 let num = "0123456789";
 let special = "!@#$%^&*()_-+=~<>?/|";
 
+//empty arrays to write the random concatentations
 let userPass=[];
 let userSetChar=[];
 
@@ -14,9 +15,13 @@ let userSetChar=[];
 let generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  //will clear the old password, if one is generated, and allow the new password to be written to the screen
   if(userPass.length > 0){
     userPass = []
   }
+
+  //Each of these will prompt the user to "OK" or "Cancel" the use of Upper, lower, numbers, and or special characters.
+  // If the user selects "OK", then the strings for each will be push to the end of the userSetChar array
   console.log("Hey you clicked the button!")
   //Prompt user for password critria
   let promptUpper = confirm("Should your password include uppercase?");
@@ -45,20 +50,23 @@ function generatePassword() {
   
   //Prompt user for password character length
   let characterNumber = prompt("How many characters should be in your new password?");
+
+  //If the user picks a character length less than 8 and more than 128 the loop will not continue
   if(characterNumber < 8  ){
     alert("Password must be atleast 8 characters")
-    
+
   } else if (characterNumber > 128){
     alert("Password can not excede 128 characters")
   
   } else {
-  // Loop to randomly assemble the characters selected by user to the number
+  // Loop to randomly assemble the characters selected by user to the number as long as the number is between 8 and 128
   for (let i = 0; i < characterNumber; i++) {
     userPass.push(userSetChar[Math.floor(Math.random() * userSetChar.length)])
     //console.log('user passw', userPass.join('+'))
     }
   }
-   
+  
+  // Places the random generated password in the test area
   let passwordText = document.querySelector("#password");
 
   passwordText.value = userPass.join('');
